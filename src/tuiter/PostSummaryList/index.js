39251cdc89d37dad986b582c2626ed98c55832db
Pragya <pragyaprashar490@gmail.com>
2022-11-20 00:42:31 -0500

    useeffect in explore

--- src/tuiter/PostSummaryList/index.js
+++ src/tuiter/PostSummaryList/index.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, {useEffect} from "react";
 import PostSummaryItem from "./PostSummaryItem.js";
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
+import {findTuitsThunk} from "../../services/tuits-thunks";
 
 const PostSummaryList = () => {
     //const postsArray = useSelector(state=>state.tuits)
     const {tuits}  = useSelector(state=>state.tuitsData)
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(findTuitsThunk());
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    },[]);
     return (
 
         <ul className="list-group">
